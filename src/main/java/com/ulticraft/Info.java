16ec08da9e0a2ffec7f29928c5290b4825840a73
danielmills <danielmillst@gmail.com>
2015-12-20 08:13:40 -0500

    Add constants

---
+++ src/main/java/com/ulticraft/Info.java
@@ -0,0 +1,8 @@
+package com.ulticraft;
+
+public class Info
+{
+	public static final String COMMAND_GLACIAL_RUSH = "glacialrush";
+	
+	public static final String PERMISSION_GOD = "glacialrush.god";
+}
