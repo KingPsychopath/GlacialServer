d6b8e46707be42ef058e90e528ab6c342cc13069
danielmills <danielmillst@gmail.com>
2015-12-28 19:03:11 -0500

    Distance2D function

--- src/main/java/com/ulticraft/uapi/FastMath.java
+++ src/main/java/com/ulticraft/uapi/FastMath.java
@@ -8,4 +8,9 @@ public class FastMath
 	{
 		return a.distanceSquared(b) <= f * f;
 	}
+	
+	public static double distance2D(Location a, Location b)
+	{
+		return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow((a.getZ() - b.getZ()), 2));
+	}
 }
