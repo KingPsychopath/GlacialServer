688465260afc1e7f40367c6a606813e2e7a9ce8c
danielmills <danielmillst@gmail.com>
2015-12-28 19:31:40 -0500

    Create a gameState to hold all of the current game data

--- src/main/java/com/ulticraft/game/GameState.java
+++ src/main/java/com/ulticraft/game/GameState.java
@@ -1,8 +1,18 @@
 package com.ulticraft.game;
 
+import com.ulticraft.GlacialRush;
+import com.ulticraft.composite.FactionMap;
 import com.ulticraft.composite.Map;
 
 public class GameState
 {
+	private GlacialRush pl;
 	private Map map;
+	private FactionMap factionMap;
+	
+	public GameState(GlacialRush pl)
+	{
+		this.pl = pl;
+		this.factionMap = new FactionMap(pl);
+	}
 }
