f87363ede3ff191bbc85494b66f038dbc29aff8a
danielmills <danielmillst@gmail.com>
2016-01-06 02:35:43 -0500

    Capture mechanics

--- src/main/java/com/glacialrush/composite/Capture.java
+++ src/main/java/com/glacialrush/composite/Capture.java
@@ -5,31 +5,187 @@ import org.bukkit.Location;
 public class Capture
 {
 	private Location location;
-	private Faction dominantFaction;
+	private Faction offense;
+	private Faction defense;
+	private Integer state;
+	private Region region;
 	
-	public Capture(Location location)
+	public Capture(Region region, Location location)
 	{
 		this.location = location;
-		this.dominantFaction = Faction.neutral();
+		this.region = region;
+		this.state = 100;
+		this.offense = null;
+		this.defense = Faction.neutral();
 	}
 	
+	public void reset()
+	{
+		offense = null;
+		defense = region.getFaction();
+		state = 100;
+	}
+	
+	public boolean enemySecured()
+	{
+		if(state == -100)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean allySecured()
+	{
+		if(state == 100)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public Faction getSecured()
+	{
+		if(state == 100)
+		{
+			return defense;
+		}
+		
+		else if(state == -100)	
+		{
+			return offense;
+		}
+		
+		else
+		{
+			return Faction.neutral();
+		}
+	}
+	
+	public void capture(Faction offense, Integer op, Integer dp, Integer opf)
+	{
+		Faction secured = getSecured();
+		
+		if(this.offense == null)
+		{
+			this.offense = offense;
+			
+			if(op > dp)
+			{
+				state--;
+			}
+			
+			else if(dp > op)
+			{
+				state++;
+			}
+		}
+		
+		else if(!this.offense.equals(offense))
+		{
+			if(op > dp)
+			{
+				if(state == 0)
+				{
+					if(opf > op - opf)
+					{
+						this.offense = offense;
+					}
+					
+					state--;
+				}
+				
+				else if(state < 0)
+				{
+					state--;
+				}
+			}
+			
+			else if(dp > op)
+			{
+				state++;
+			}
+		}
+		
+		else if(this.offense.equals(offense))
+		{
+			if(op > dp)
+			{
+				state--;
+			}
+			
+			else if(dp > op)
+			{
+				state++;
+			}
+		}
+		
+		if(state < -100)
+		{
+			state = -100;
+		}
+		
+		if(state > 100)
+		{
+			this.offense = null;
+			state = 100;
+		}
+		
+		if(!secured.equals(getSecured()))
+		{
+			region.capture(this, getSecured());
+		}
+	}
+
 	public Location getLocation()
 	{
 		return location;
 	}
-	
+
 	public void setLocation(Location location)
 	{
 		this.location = location;
 	}
-	
-	public Faction getDominantFaction()
+
+	public Faction getOffense()
 	{
-		return dominantFaction;
+		return offense;
 	}
-	
-	public void setDominantFaction(Faction dominantFaction)
+
+	public void setOffense(Faction offense)
 	{
-		this.dominantFaction = dominantFaction;
+		this.offense = offense;
+	}
+
+	public Faction getDefense()
+	{
+		return defense;
+	}
+
+	public void setDefense(Faction defense)
+	{
+		this.defense = defense;
+	}
+
+	public Integer getState()
+	{
+		return state;
+	}
+
+	public void setState(Integer state)
+	{
+		this.state = state;
+	}
+
+	public Region getRegion()
+	{
+		return region;
+	}
+
+	public void setRegion(Region region)
+	{
+		this.region = region;
 	}
 }
