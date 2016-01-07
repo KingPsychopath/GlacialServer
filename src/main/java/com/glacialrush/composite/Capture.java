e0a20b901b20a5d69ecf464aa3c0be8a1478e87a
danielmills <danielmillst@gmail.com>
2016-01-07 02:17:27 -0500

    Capture point ticking overhaul

--- src/main/java/com/glacialrush/composite/Capture.java
+++ src/main/java/com/glacialrush/composite/Capture.java
@@ -1,8 +1,13 @@
 package com.glacialrush.composite;
 
+import java.util.Iterator;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import com.glacialrush.composite.Hunk.HunkFace;
+import com.glacialrush.game.Game;
+import com.glacialrush.game.event.FactionCaptureEvent;
 import com.glacialrush.xapi.UList;
+import com.glacialrush.xapi.UMap;
 
 public class Capture
 {
@@ -11,8 +16,9 @@ public class Capture
 	private Faction defense;
 	private Integer state;
 	private Region region;
+	private Faction secured;
 	
-	private static final int size = 20;
+	private static final int size = 1;
 	
 	public Capture(Region region, Location location)
 	{
@@ -21,11 +27,189 @@ public class Capture
 		this.state = 100;
 		this.offense = null;
 		this.defense = Faction.neutral();
+		this.secured = Faction.neutral();
 	}
 	
 	public UList<Player> getPlayers()
 	{
-		return region.getGame().getEventRippler().getPlayers(this);
+		UList<Player> players = new UList<Player>();
+		
+		for(Player i : region.getGame().pl().onlinePlayers())
+		{
+			if(getRegion().contains(i))
+			{
+				if(location.distanceSquared(i.getLocation()) < 25)
+				{
+					players.add(i);
+				}
+			}
+		}
+		
+		return players;
+	}
+	
+	public void tick(Game g)
+	{
+		UMap<Player, Faction> players = g.getState().getFactionMap().getFactions(getPlayers());
+		UMap<Faction, Integer> points = new UMap<Faction, Integer>();
+		
+		if(players.isEmpty())
+		{
+			return;
+		}
+		
+		for(Faction i : Faction.all())
+		{
+			if(!canCapture(i))
+			{
+				Iterator<Player> it = players.keySet().iterator();
+				
+				while(it.hasNext())
+				{
+					Player p = it.next();
+					
+					if(players.get(p).equals(i))
+					{
+						players.remove(p);
+					}
+				}
+			}
+		}
+		
+		for(Player i : players.keySet())
+		{
+			if(!points.containsKey(players.get(i)))
+			{
+				points.put(players.get(i), 1);
+			}
+			
+			else
+			{
+				points.put(players.get(i), points.get(players.get(i)) + 1);
+			}
+		}
+		
+		int mdefense = 0;
+		int moffense = 0;
+		int moffenseMain = 0;
+		int moffenseStray = 0;
+		
+		int a = 0;
+		int b = 0;
+		
+		Faction fa = null;
+		Faction fb = null;
+		Faction foffenseMain = null;
+		Faction foffenseStray = null;
+		
+		if(points.containsKey(defense))
+		{
+			mdefense = points.get(defense);
+		}
+		
+		for(Faction i : points.keySet())
+		{
+			if(!i.equals(defense))
+			{
+				if(fa == null)
+				{
+					fa = i;
+					a++;
+				}
+				
+				else if(fa.equals(i))
+				{
+					a++;
+				}
+				
+				else
+				{
+					fb = i;
+					b++;
+				}
+			}
+		}
+		
+		if(a > b)
+		{
+			foffenseMain = fa;
+			foffenseStray = fb;
+			moffenseMain = a;
+			moffenseStray = b;
+		}
+		
+		else if(b > a)
+		{
+			foffenseMain = fb;
+			foffenseStray = fa;
+			moffenseMain = b;
+			moffenseStray = a;
+		}
+		
+		else
+		{
+			foffenseMain = fa;
+			foffenseStray = fb;
+			moffenseMain = a;
+			moffenseStray = b;
+		}
+		
+		offense = foffenseMain;
+		moffense = moffenseMain + moffenseStray;
+		
+		if(moffense == 0 && mdefense != 0)
+		{
+			return;
+		}
+		
+		if(mdefense > moffense)
+		{
+			state += size;
+			g.pl().callEvent(new FactionCaptureEvent(this, offense, moffense, mdefense, moffenseStray));
+		}
+		
+		else if(mdefense < moffense)
+		{
+			state -= size;
+			g.pl().callEvent(new FactionCaptureEvent(this, offense, moffense, mdefense, moffenseStray));
+		}
+		
+		if(state > 100)
+		{
+			state = 100;
+			offense = null;
+			secured = defense;
+		}
+		
+		if(state < -100)
+		{
+			state = -100;
+			region.capture(this, offense);
+			secured = offense;
+		}
+	}
+	
+	public void setSecured(Faction secured)
+	{
+		this.secured = secured;
+	}
+	
+	public boolean canCapture(Faction f)
+	{
+		if(region.getFaction().equals(f))
+		{
+			return true;
+		}
+		
+		for(HunkFace i : HunkFace.values())
+		{
+			if(region.getMap().getRegion(region.getHunk().getRelative(i)) != null && f.equals(region.getMap().getRegion(region.getHunk().getRelative(i)).getFaction()))
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	public void reset()
@@ -57,95 +241,7 @@ public class Capture
 	
 	public Faction getSecured()
 	{
-		if(state == 100)
-		{
-			return defense;
-		}
-		
-		else if(state == -100)
-		{
-			return offense;
-		}
-		
-		else
-		{
-			return Faction.neutral();
-		}
-	}
-	
-	public void capture(Faction offense, Integer op, Integer dp, Integer opf)
-	{
-		Faction secured = getSecured();
-		
-		if(this.offense == null)
-		{
-			this.offense = offense;
-			
-			if(op > dp)
-			{
-				state -= size;
-			}
-			
-			else if(dp > op)
-			{
-				state += size;
-			}
-		}
-		
-		else if(!this.offense.equals(offense))
-		{
-			if(op > dp)
-			{
-				if(state == 0)
-				{
-					if(opf > op - opf)
-					{
-						this.offense = offense;
-					}
-					
-					state -= size;
-				}
-				
-				else if(state < 0)
-				{
-					state -= size;
-				}
-			}
-			
-			else if(dp > op)
-			{
-				state += size;
-			}
-		}
-		
-		else if(this.offense.equals(offense))
-		{
-			if(op > dp)
-			{
-				state -= size;
-			}
-			
-			else if(dp > op)
-			{
-				state += size;
-			}
-		}
-		
-		if(state < -100)
-		{
-			state = -100;
-		}
-		
-		if(state > 100)
-		{
-			this.offense = null;
-			state = 100;
-		}
-		
-		if(!secured.equals(getSecured()))
-		{
-			region.capture(this, getSecured());
-		}
+		return secured;
 	}
 	
 	public Location getLocation()
