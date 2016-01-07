68603c8ef5d09a97aee5588f312dc241569a131a
danielmills <danielmillst@gmail.com>
2016-01-07 02:18:33 -0500

    Remove capturing from the event rippler

--- src/main/java/com/glacialrush/game/component/EventRippler.java
+++ src/main/java/com/glacialrush/game/component/EventRippler.java
@@ -5,18 +5,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import com.glacialrush.composite.Capture;
-import com.glacialrush.composite.Faction;
-import com.glacialrush.composite.Hunk;
 import com.glacialrush.composite.Region;
 import com.glacialrush.game.Game;
 import com.glacialrush.game.GameComponent;
 import com.glacialrush.game.Tickrement;
 import com.glacialrush.game.Tickreval;
-import com.glacialrush.game.event.EnterCapturePointEvent;
-import com.glacialrush.game.event.ExitCapturePointEvent;
 import com.glacialrush.game.event.FactionCaptureEvent;
 import com.glacialrush.xapi.UList;
 import com.glacialrush.xapi.UMap;
@@ -40,6 +35,17 @@ public class EventRippler implements GameComponent, Listener
 	@Override
 	public void onTick(Game g)
 	{
+		for(Region i : g.getState().getMap().getRegions())
+		{
+			for(Capture j : i.getCaptures())
+			{
+				if(!j.getPlayers().isEmpty())
+				{
+					j.tick(g);
+				}
+			}
+		}
+		
 		for(Capture i : events.keySet())
 		{
 			g.pl().callEvent(events.get(i));
@@ -47,142 +53,18 @@ public class EventRippler implements GameComponent, Listener
 		
 		events.clear();
 	}
-
+	
 	@Override
 	public void onStart(Game g)
 	{
 		game = g;
 		g.pl().register(this);
 	}
-
+	
 	@Override
 	public void onStop(Game g)
 	{
 		g.pl().unRegister(this);
-	}
-	
-	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent e)
-	{
-		if(game.getState().getMap().contains(e.getFrom()) && !game.getState().getMap().contains(e.getTo()))
-		{
-			e.setCancelled(true);
-		}
-		
-		if(!game.getState().getMap().contains(e.getFrom()) && game.getState().getMap().contains(e.getTo()))
-		{
-			e.setCancelled(true);
-		}
-		
-		if(game.getState().getMap().contains(e.getTo()))
-		{
-			Region r = game.getState().getMap().getRegion(new Hunk(e.getTo()));
-			
-			for(Capture i : r.getCaptures())
-			{
-				if(!offensives.containsKey(i))
-				{
-					offensives.put(i, new UList<Player>());
-				}
-				
-				if(e.getTo().distanceSquared(i.getLocation()) < 25)
-				{
-					if(!offensives.get(i).contains(e.getPlayer()))
-					{
-						offensives.get(i).add(e.getPlayer());
-						game.pl().callEvent(new EnterCapturePointEvent(i, e.getPlayer()));
-					}
-				}
-				
-				else
-				{
-					if(offensives.get(i).contains(e.getPlayer()))
-					{
-						offensives.get(i).remove(e.getPlayer());
-						game.pl().callEvent(new ExitCapturePointEvent(i, e.getPlayer()));
-					}
-				}
-				
-				Faction def = i.getDefense();
-				Faction fac = null;
-				Faction faf = null;
-				int op = 0;
-				int dp = 0;
-				int opf = 0;
-				boolean ok = true;
-				
-				for(Player j : offensives.get(i))
-				{
-					Faction f = game.getState().getFactionMap().getFaction(j);
-					
-					if(f == null)
-					{
-						continue;
-					}
-					
-					if(f.equals(def))
-					{
-						dp++;
-					}
-					
-					else
-					{
-						if(fac == null)
-						{
-							fac = f;
-							op++;
-						}
-						
-						else
-						{
-							if(f.equals(fac))
-							{
-								op++;
-							}
-							
-							else
-							{
-								if(faf == null)
-								{
-									faf = f;
-								}
-								
-								opf++;
-							}
-						}
-					}
-				}
-				
-				if(op < opf)
-				{
-					fac = faf;
-					int k = op;
-					int l = opf;
-					
-					op = k + l;
-					opf = op;
-				}
-				
-				else if(op > opf)
-				{
-					int k = op;
-					int l = opf;
-					
-					op = k + l;
-				}
-				
-				else
-				{
-					ok = false;
-				}
-				
-				if(ok)
-				{
-					FactionCaptureEvent fce = new FactionCaptureEvent(i, fac, op, dp, opf);
-					events.put(i, fce);
-				}
-			}
-		}
 	}
 	
 	@EventHandler
