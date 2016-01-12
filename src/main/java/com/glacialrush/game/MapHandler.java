3e9f3d802642d66abb218513d952d13a1ca4e658
danielmills <danielmillst@gmail.com>
2016-01-11 20:30:32 -0500

    Create Map Handler

---
+++ src/main/java/com/glacialrush/game/MapHandler.java
@@ -0,0 +1,41 @@
+package com.glacialrush.game;
+
+import com.glacialrush.GlacialServer;
+
+public class MapHandler extends GlacialHandler
+{
+	public MapHandler(GlacialServer pl)
+	{
+		super(pl);
+	}
+	
+	@Override
+	public void start()
+	{
+		
+	}
+
+	@Override
+	public void begin()
+	{
+		
+	}
+
+	@Override
+	public void finish()
+	{
+		
+	}
+
+	@Override
+	public void stop()
+	{
+		
+	}
+
+	@Override
+	public void tick()
+	{
+		
+	}
+}
