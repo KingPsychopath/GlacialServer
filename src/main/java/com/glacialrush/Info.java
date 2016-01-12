2b0749e1e6237dc9a2393e0c75043806759af221
danielmills <danielmillst@gmail.com>
2016-01-11 20:31:10 -0500

    Create info file

---
+++ src/main/java/com/glacialrush/Info.java
@@ -0,0 +1,12 @@
+package com.glacialrush;
+
+public class Info
+{
+	public static final String NAME = "Glacial Rush";
+	public static final String VERSION = "1.0.354D";
+	public static final String[] AUTHORS = new String[] {"Daniel Mills (cyberpwn)"};
+	
+	public static final String CMD_GLACIALRUSH = "glacialrush";
+	
+	public static final String PERM_ADMIN = "glacialrush.admin";
+}
