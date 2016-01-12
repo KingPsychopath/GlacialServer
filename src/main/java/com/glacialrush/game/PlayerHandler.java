269b122718a1177c32f1361346aa859b3efaf98d
danielmills <danielmillst@gmail.com>
2016-01-11 20:30:42 -0500

    Player Handler

---
+++ src/main/java/com/glacialrush/game/PlayerHandler.java
@@ -0,0 +1,41 @@
+package com.glacialrush.game;
+
+import com.glacialrush.GlacialServer;
+
+public class PlayerHandler extends GlacialHandler
+{
+	public PlayerHandler(GlacialServer pl)
+	{
+		super(pl);
+	}
+	
+	@Override
+	public void start()
+	{
+		
+	}
+
+	@Override
+	public void begin()
+	{
+		
+	}
+
+	@Override
+	public void finish()
+	{
+		
+	}
+
+	@Override
+	public void stop()
+	{
+		
+	}
+
+	@Override
+	public void tick()
+	{
+		
+	}
+}
