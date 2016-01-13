882fd908260396cdecc931e847b2d6bf74168989
danielmills <danielmillst@gmail.com>
2016-01-12 21:25:37 -0500

    More map functions

--- src/main/java/com/glacialrush/composite/Map.java
+++ src/main/java/com/glacialrush/composite/Map.java
@@ -1,10 +1,12 @@
 package com.glacialrush.composite;
 
+import java.util.Collections;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import com.glacialrush.GlacialServer;
 import com.glacialrush.api.object.GList;
+import com.glacialrush.api.object.GMap;
 import com.glacialrush.api.thread.GlacialTask;
 import com.glacialrush.component.CommandController;
 
@@ -15,6 +17,7 @@ public class Map
 	protected String name;
 	protected GList<Region> regions;
 	protected GlacialServer pl;
+	protected GMap<Faction, Region> spawns;
 	
 	public Map(GlacialServer pl, World world, String name)
 	{
@@ -23,6 +26,7 @@ public class Map
 		this.world = world;
 		this.pl = pl;
 		this.faction = Faction.neutral();
+		this.spawns = new GMap<Faction, Region>();
 	}
 	
 	public void addRegion(Player p)
@@ -94,6 +98,14 @@ public class Map
 		return false;
 	}
 	
+	public void accent()
+	{
+		for(Region i : regions)
+		{
+			i.accent();
+		}
+	}
+	
 	public void accent(Faction faction)
 	{
 		for(Region i : regions)
@@ -103,9 +115,170 @@ public class Map
 		}
 	}
 	
+	public GMap<MapCorner, Region> getCorners()
+	{
+		GMap<MapCorner, Region> corners = new GMap<MapCorner, Region>();
+		
+		for(MapCorner i : MapCorner.values())
+		{
+			int x = i.isX() ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+			int z = i.isZ() ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+			Region c = null;
+			
+			for(Region j : regions)
+			{
+				if(i.isX())
+				{
+					if(j.getX() > x)
+					{
+						x = j.getX();
+						
+						for(Region k : regions)
+						{
+							if(j.getX() == x)
+							{
+								if(i.isZ())
+								{
+									if(k.getZ() > z)
+									{
+										z = k.getZ();
+										c = k;
+									}
+								}
+								
+								else
+								{
+									if(k.getZ() < z)
+									{
+										z = k.getZ();
+										c = k;
+									}
+								}
+							}
+						}
+					}
+				}
+				
+				else
+				{
+					if(j.getX() < x)
+					{
+						x = j.getX();
+						
+						for(Region k : regions)
+						{
+							if(j.getX() == x)
+							{
+								if(i.isZ())
+								{
+									if(k.getZ() > z)
+									{
+										z = k.getZ();
+										c = k;
+									}
+								}
+								
+								else
+								{
+									if(k.getZ() < z)
+									{
+										z = k.getZ();
+										c = k;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			if(c != null)
+			{
+				corners.put(i, c);
+			}
+		}
+		
+		return corners;
+	}
+	
+	public void randomizeSpawns()
+	{
+		GMap<MapCorner, Region> corners = getCorners();
+		
+		if(corners.size() < 3)
+		{
+			pl.f("Not enough regions to randomize spawns");
+			return;
+		}
+		
+		GList<Region> spawns = new GList<Region>();
+		GList<Faction> factions = Faction.all();
+		
+		for(Region i : corners.values())
+		{
+			spawns.add(i);
+		}
+		
+		Collections.shuffle(spawns);
+		Collections.shuffle(factions);
+		
+		this.spawns.clear();
+		
+		for(Faction i : factions)
+		{
+			this.spawns.put(i, spawns.get(0));
+			spawns.remove(0);
+		}
+	}
+	
+	public void neutralize()
+	{
+		for(Region i : regions)
+		{
+			i.setFaction(Faction.neutral());
+		}
+	}
+	
 	public void accentEvenley()
 	{
+		neutralize();
+		randomizeSpawns();
 		
+		for(Faction i : spawns.keySet())
+		{
+			spawns.get(i).setFaction(i);
+			spawns.get(i).accent();
+		}
+		
+		//TODO Accent it bro
+	}
+	
+	public GList<Region> getRegions(Faction faction)
+	{
+		GList<Region> reg = new GList<Region>();
+		
+		for(Region i : regions)
+		{
+			if(i.getFaction().equals(faction))
+			{
+				reg.add(i);
+			}
+		}
+		
+		return reg;
+	}
+	
+	public boolean hasNeutralRegions()
+	{
+		for(Region i : regions)
+		{
+			if(i.getFaction().equals(Faction.neutral()))
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	public boolean contains(Player player)
@@ -221,42 +394,42 @@ public class Map
 		
 		return false;
 	}
-
+	
 	public World getWorld()
 	{
 		return world;
 	}
-
+	
 	public void setWorld(World world)
 	{
 		this.world = world;
 	}
-
+	
 	public Faction getFaction()
 	{
 		return faction;
 	}
-
+	
 	public void setFaction(Faction faction)
 	{
 		this.faction = faction;
 	}
-
+	
 	public String getName()
 	{
 		return name;
 	}
-
+	
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-
+	
 	public GList<Region> getRegions()
 	{
 		return regions;
 	}
-
+	
 	public void setRegions(GList<Region> regions)
 	{
 		this.regions = regions;
