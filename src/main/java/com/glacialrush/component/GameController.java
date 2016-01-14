4331fa239bb8628de82be2fe9b93fdd2ef07c276
danielmills <danielmillst@gmail.com>
2016-01-14 00:29:44 -0500

    Game controller debugging

--- src/main/java/com/glacialrush/component/GameController.java
+++ src/main/java/com/glacialrush/component/GameController.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import org.bukkit.Location;
 import com.glacialrush.GlacialServer;
 import com.glacialrush.api.component.Controller;
+import com.glacialrush.api.dispatch.notification.Notification;
+import com.glacialrush.api.dispatch.notification.NotificationPriority;
 import com.glacialrush.api.object.GList;
 import com.glacialrush.api.thread.GlacialTask;
 import com.glacialrush.composite.Faction;
@@ -12,6 +14,7 @@ import com.glacialrush.game.GameHandler;
 import com.glacialrush.game.MapHandler;
 import com.glacialrush.game.MarketHandler;
 import com.glacialrush.game.PlayerHandler;
+import net.md_5.bungee.api.ChatColor;
 
 public class GameController extends Controller
 {
@@ -43,19 +46,26 @@ public class GameController extends Controller
 	
 	public void start()
 	{
+		s("Starting Game");
+		
 		if(maps.isEmpty())
 		{
-			pl.f("Game Start: Failed: No maps!");
+			f("Game Start: Failed: No maps!");
+			return;
 		}
 		
 		Collections.shuffle(maps);
+		o("Picking Map");
 		map = maps.get(0);
+		s("Selected Map: " + map.getName());
 		
 		for(GameHandler i : handlers)
 		{
 			i.start();
+			v("Started Handler: " + ChatColor.RED + i.getClass().getSimpleName());
 		}
 		
+		v("Setting up map...");
 		map.setup();
 		
 		pl.newThread(new GlacialTask()
@@ -64,6 +74,7 @@ public class GameController extends Controller
 			{
 				if(map.isAccenting() || map.isBuilding())
 				{
+					o("Waiting for Map to finish Setting up...");
 					setDelay(20);
 				}
 				
@@ -82,6 +93,7 @@ public class GameController extends Controller
 		for(GameHandler i : handlers)
 		{
 			i.begin();
+			v("Begin Handler: " + ChatColor.RED + i.getClass().getSimpleName());
 		}
 		
 		running = true;
@@ -92,6 +104,7 @@ public class GameController extends Controller
 		for(GameHandler i : handlers)
 		{
 			i.finish();
+			v("Finish Handler: " + ChatColor.RED + i.getClass().getSimpleName());
 		}
 	}
 	
@@ -102,9 +115,11 @@ public class GameController extends Controller
 		for(GameHandler i : handlers)
 		{
 			i.stop();
+			v("Stop Handler: " + ChatColor.RED + i.getClass().getSimpleName());
 		}
 		
 		map.accent(Faction.neutral());
+		o("Post Accenting Map: " + map.getName());
 		
 		pl.newThread(new GlacialTask()
 		{
@@ -113,6 +128,7 @@ public class GameController extends Controller
 				if(map.isAccenting())
 				{
 					setDelay(20);
+					o("Waiting for map to finish accenting...");
 				}
 				
 				else
@@ -128,11 +144,52 @@ public class GameController extends Controller
 	public void preEnable()
 	{
 		super.preEnable();
+		
+		o("Starting Progress Thread");
+		
+		pl.newThread(new GlacialTask()
+		{
+			@Override
+			public void run()
+			{
+				if(isRunning())
+				{
+					setDelay(100);
+					return;
+				}
+				
+				String prog = "";
+				
+				for(Map i : maps)
+				{
+					if(i.isBuilding())
+					{
+						String build = ChatColor.BLUE + i.getName() + ": " + ChatColor.YELLOW + i.buildProgress() + "%" + " ";
+						prog = prog + build;
+					}
+					
+					if(i.isAccenting())
+					{
+						String accent = ChatColor.GREEN + i.getName() + ": " + ChatColor.YELLOW + i.accentProgress() + "%" + " ";
+						prog = prog + accent;
+					}
+				}
+				
+				if(!prog.equals(""))
+				{
+					Notification n = new Notification().setSubSubTitle(prog).setDelay(false).setPriority(NotificationPriority.LOWEST);
+					
+					((GlacialServer) pl).getNotificationController().dispatch(n);
+				}
+			}
+		});
 	}
 	
 	public void postEnable()
 	{
 		super.postEnable();
+		
+		o("Registering Handlers...");
 		
 		mapHandler = new MapHandler((GlacialServer) pl);
 		marketHandler = new MarketHandler((GlacialServer) pl);
@@ -160,6 +217,8 @@ public class GameController extends Controller
 				}
 			}
 		});
+		
+		start();
 	}
 	
 	public void preDisable()
@@ -210,77 +269,77 @@ public class GameController extends Controller
 		
 		return null;
 	}
-
+	
 	public boolean isRunning()
 	{
 		return running;
 	}
-
+	
 	public void setRunning(boolean running)
 	{
 		this.running = running;
 	}
-
+	
 	public Map getMap()
 	{
 		return map;
 	}
-
+	
 	public void setMap(Map map)
 	{
 		this.map = map;
 	}
-
+	
 	public GList<Map> getMaps()
 	{
 		return maps;
 	}
-
+	
 	public void setMaps(GList<Map> maps)
 	{
 		this.maps = maps;
 	}
-
+	
 	public GList<GameHandler> getHandlers()
 	{
 		return handlers;
 	}
-
+	
 	public void setHandlers(GList<GameHandler> handlers)
 	{
 		this.handlers = handlers;
 	}
-
+	
 	public long getCycleTime()
 	{
 		return cycleTime;
 	}
-
+	
 	public void setCycleTime(long cycleTime)
 	{
 		this.cycleTime = cycleTime;
 	}
-
+	
 	public long getCycles()
 	{
 		return cycles;
 	}
-
+	
 	public void setCycles(long cycles)
 	{
 		this.cycles = cycles;
 	}
-
+	
 	public MapHandler getMapHandler()
 	{
 		return mapHandler;
 	}
-
+	
 	public MarketHandler getMarketHandler()
 	{
 		return marketHandler;
 	}
-
+	
 	public PlayerHandler getPlayerHandler()
 	{
 		return playerHandler;
