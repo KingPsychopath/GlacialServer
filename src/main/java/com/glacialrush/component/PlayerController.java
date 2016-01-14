33154c618a7b14385887d9cdf8d42e0db59bd082
danielmills <danielmillst@gmail.com>
2016-01-14 02:40:59 -0500

    Player Disabling :dancer:

--- src/main/java/com/glacialrush/component/PlayerController.java
+++ src/main/java/com/glacialrush/component/PlayerController.java
@@ -1,13 +1,26 @@
 package com.glacialrush.component;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import com.glacialrush.GlacialServer;
 import com.glacialrush.api.component.Controller;
+import com.glacialrush.api.object.GList;
 
 public class PlayerController extends Controller
 {
+	private GList<Player> disabled;
+	
 	public PlayerController(GlacialServer pl)
 	{
 		super(pl);
+		
+		this.disabled = new GList<Player>();
 	}
 	
 	public void preEnable()
@@ -28,5 +41,43 @@ public class PlayerController extends Controller
 	public void postDisable()
 	{
 		super.postDisable();
+	}
+	
+	public void disable(Player p)
+	{
+		if(disabled.contains(p))
+		{
+			return;
+		}
+		
+		disabled.add(p);
+		p.setGameMode(GameMode.ADVENTURE);
+		p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100000, 50));
+		p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100000, 50));
+		p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 100000, 50));
+	}
+	
+	public void enable(Player p)
+	{
+		disabled.remove(p);
+		p.setGameMode(GameMode.ADVENTURE);
+		p.setHealth(p.getMaxHealth());
+		p.removePotionEffect(PotionEffectType.BLINDNESS);
+		p.removePotionEffect(PotionEffectType.SLOW);
+		p.removePotionEffect(PotionEffectType.SLOW_DIGGING);
+	}
+	
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent e)
+	{
+		if(!e.getEntityType().equals(EntityType.PLAYER))
+		{
+			return;
+		}
+		
+		if(disabled.contains((Player)e.getEntity()))
+		{
+			e.setCancelled(true);
+		}
 	}
 }
