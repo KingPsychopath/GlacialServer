0c3b5e04ed963b99ab5f58ffab56c6ce40955d2b
danielmills <danielmillst@gmail.com>
2016-01-14 08:59:59 -0500

    Capture progress & checking utils

--- src/main/java/com/glacialrush/composite/Capture.java
+++ src/main/java/com/glacialrush/composite/Capture.java
@@ -5,9 +5,11 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import com.glacialrush.GlacialServer;
 import com.glacialrush.api.object.GList;
 import com.glacialrush.composite.Job.JobStatus;
+import com.glacialrush.xapi.FastMath;
 
 public class Capture
 {
@@ -37,7 +39,6 @@ public class Capture
 		return accentJob.getStatus().equals(JobStatus.RUNNING) || accentJob.getStatus().equals(JobStatus.WAITING);
 	}
 	
-	
 	public void accent()
 	{
 		if(isAccenting())
@@ -48,7 +49,7 @@ public class Capture
 		Block b = location.getBlock();
 		DyeColor dye = getSecured().getDyeColor();
 		accentJob = new Job("Region [" + region.x + ", " + region.z + "]: " + region.getName() + " Capture Accent[" + getSecured().getName() + "]", pl.getJobController());
-
+		
 		accentJob.add(new Manipulation(b.getRelative(BlockFace.UP).getLocation(), Material.STAINED_GLASS));
 		accentJob.add(new Manipulation(b.getRelative(BlockFace.UP).getLocation(), dye));
 		accentJob.add(new Manipulation(b.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH).getLocation(), dye));
@@ -87,6 +88,36 @@ public class Capture
 		accentJob.add(new Manipulation(b.getRelative(BlockFace.UP).getRelative(BlockFace.UP).getLocation(), dye));
 		
 		pl.getJobController().addJob(accentJob);
+	}
+	
+	public void offend()
+	{
+		progress -= 5;
+		
+		if(progress > 100)
+		{
+			progress = 100;
+		}
+		
+		if(progress < 0)
+		{
+			progress = 0;
+		}
+	}
+	
+	public void defend()
+	{
+		progress += 5;
+		
+		if(progress > 100)
+		{
+			progress = 100;
+		}
+		
+		if(progress < 0)
+		{
+			progress = 0;
+		}
 	}
 	
 	public GList<Manipulation> buildTask()
@@ -135,62 +166,94 @@ public class Capture
 		
 		return jobs;
 	}
-
+	
+	public void reset()
+	{
+		reset(region.getFaction());
+	}
+	
+	public void reset(Faction f)
+	{
+		setSecured(f);
+		setOffense(null);
+		setProgress(100);
+	}
+	
+	public GList<Player> getPlayers()
+	{
+		GList<Player> players = new GList<Player>();
+		
+		for(Player i : pl.onlinePlayers())
+		{
+			if(contains(i))
+			{
+				players.add(i);
+			}
+		}
+		
+		return players;
+	}
+	
+	public boolean contains(Player p)
+	{
+		return FastMath.isInRadius(p.getLocation(), getLocation(), 6);
+	}
+	
 	public Region getRegion()
 	{
 		return region;
 	}
-
+	
 	public void setRegion(Region region)
 	{
 		this.region = region;
 	}
-
+	
 	public Map getMap()
 	{
 		return map;
 	}
-
+	
 	public void setMap(Map map)
 	{
 		this.map = map;
 	}
-
+	
 	public Faction getSecured()
 	{
 		return secured;
 	}
-
+	
 	public void setSecured(Faction secured)
 	{
 		this.secured = secured;
 	}
-
+	
 	public Faction getOffense()
 	{
 		return offense;
 	}
-
+	
 	public void setOffense(Faction offense)
 	{
 		this.offense = offense;
 	}
-
+	
 	public Integer getProgress()
 	{
 		return progress;
 	}
-
+	
 	public void setProgress(Integer progress)
 	{
 		this.progress = progress;
 	}
-
+	
 	public Location getLocation()
 	{
 		return location;
 	}
-
+	
 	public void setLocation(Location location)
 	{
 		this.location = location;
