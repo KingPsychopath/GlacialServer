2396d0647b8708a9313f180ef277c2acbff014ed
danielmills <danielmillst@gmail.com>
2016-01-16 09:27:20 -0500

    Change RegionCaptureEvent Super class to Region Event

--- src/main/java/com/glacialrush/game/event/FactionRegionCaptureEvent.java
+++ src/main/java/com/glacialrush/game/event/FactionRegionCaptureEvent.java
@@ -1,16 +1,16 @@
 package com.glacialrush.game.event;
 
 import com.glacialrush.component.GameController;
-import com.glacialrush.composite.Capture;
 import com.glacialrush.composite.Faction;
+import com.glacialrush.composite.Region;
 
-public class FactionRegionCaptureEvent extends CaptureEvent
+public class FactionRegionCaptureEvent extends RegionEvent
 {
 	private Faction faction;
 	
-	public FactionRegionCaptureEvent(GameController gameController, Capture capture, Faction faction)
+	public FactionRegionCaptureEvent(GameController gameController, Region region, Faction faction)
 	{
-		super(gameController, capture);
+		super(gameController, region);
 		this.faction = faction;
 	}
 
