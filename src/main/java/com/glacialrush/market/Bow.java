a50af6c707defe15f1b18f421a01aa5b766b0875
danielmills <danielmillst@gmail.com>
2016-01-17 14:41:44 -0500

    Add Bows from weapons

---
+++ src/main/java/com/glacialrush/market/Bow.java
@@ -0,0 +1,11 @@
+package com.glacialrush.market;
+
+import com.glacialrush.component.MarketController;
+
+public class Bow extends PrimaryWeapon
+{
+	public Bow(String name, MarketController controller)
+	{
+		super(name, controller);
+	}
+}
