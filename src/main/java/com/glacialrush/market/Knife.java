e164166bda02a81ef105babf10cbb99669cb4f48
danielmills <danielmillst@gmail.com>
2016-01-17 14:42:42 -0500

    Create a knife item

---
+++ src/main/java/com/glacialrush/market/Knife.java
@@ -0,0 +1,11 @@
+package com.glacialrush.market;
+
+import com.glacialrush.component.MarketController;
+
+public class Knife extends MeleeWeapon
+{
+	public Knife(String name, MarketController controller)
+	{
+		super(name, controller);
+	}
+}
