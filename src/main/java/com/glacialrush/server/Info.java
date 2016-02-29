5e177044126b4d0d24b38e591de193d3af68e4a8
danielmills <danielmillst@gmail.com>
2016-02-29 12:37:50 -0500

    Remote command in Info

--- src/main/java/com/glacialrush/server/Info.java
+++ src/main/java/com/glacialrush/server/Info.java
@@ -36,6 +36,7 @@ public class Info
 	public static final String CMD_INVITE = "invite";
 	public static final String CMD_OBJECTIVE = "objective";
 	public static final String CMD_HELP = "help";
+	public static final String CMD_REMOTE = "remote";
 	
 	public static final String PERM_ADMIN = "glacialrush.admin";
 	public static final String PERM_BUILDER = "glacialrush.builder";
