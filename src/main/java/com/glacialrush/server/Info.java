673c4cfd978fcfcc3b6f11b2ad5dd65e55f0213e
danielmills <danielmillst@gmail.com>
2016-03-09 03:49:37 -0500

    define ping command

--- src/main/java/com/glacialrush/server/Info.java
+++ src/main/java/com/glacialrush/server/Info.java
@@ -38,6 +38,7 @@ public class Info
 	public static final String CMD_HELP = "help";
 	public static final String CMD_REMOTE = "remote";
 	public static final String CMD_STATS = "stats";
+	public static final String CMD_PING = "ping";
 	
 	public static final String PERM_ADMIN = "glacialrush.admin";
 	public static final String PERM_BUILDER = "glacialrush.builder";
